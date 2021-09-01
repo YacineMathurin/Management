50bdd1340a07f33099f8489fd363368ee77da61e
abdouramane <mahamane_abdouramane@qenvi.com>
2021-09-01 16:22:06 +0200

    Adding a chart

--- src/PageAide.js
+++ src/PageAide.js
@@ -1,4 +1,3 @@
-
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Container from '@material-ui/core/Container';
@@ -6,17 +5,10 @@ import Grid from '@material-ui/core/Grid';
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import React from 'react';
-import YouTube from 'react-youtube';
-import pdf_memo_img from './assets/DTF_MEMO.jpg';
-import pdf_memo from './assets/DTF_MEMO.pdf';
 import * as Const from './Constant';
 import CardHeader from '@material-ui/core/CardHeader';
-import { TableRow, TableCell } from '@material-ui/core';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TestLineChart from './TestLineChart';
+import ReactApexChart from "react-apexcharts";
+
 
 class PageAide extends React.Component {
   constructor(props) {
@@ -25,7 +17,7 @@ class PageAide extends React.Component {
       apiKey : props.apiKey,
       detail: "",
       synthese : null,
-      details : null,
+      details : null, mapBat : [],mapDat : [],
       robot : this.props.showDetailsMetrics
     }
   }
@@ -36,12 +28,27 @@ class PageAide extends React.Component {
   );
 
   provideHeartBeats(){
+    var MAP =[]
+    var MAPD =[]  
+   
+    
     fetch(Const.URL_WS_ROBOT_HEARTS + `?robot=${this.state.robot}`)
     .then(res => res.json())
     .then((data) => {
-      console.log("je suis là")
-        this.setState({ details: data})
-      
+    
+        console.log("je fais un tableau des dateHeures et du niveau de batterie")
+        
+        data.reverse().map((donnee) => { 
+          var d= new Date(donnee.TIMESTAMP * 1000).toLocaleString()
+          MAP.push(donnee.BAT_LEVEL)
+          MAPD.push(d)  
+        })
+        this.setState({ mapBat: MAP,
+                        mapDat: MAPD
+                      })
+        console.log('MAP', MAP)
+        console.log('MAPD', MAPD)
+             
     })
     .catch((error) => {
       console.log('Request failed', error)
@@ -54,15 +61,50 @@ class PageAide extends React.Component {
   
 
   render() {
+  
+   var series = [{
+      name: "Level",
+      data:  this.state.mapBat
+   }]
+  var options= {
+    chart: {
+      height: 350,
+      type: 'line',
+      zoom: {
+        enabled: false
+      }
+    },
+    dataLabels: {
+      enabled: true
+    },
+    stroke: {
+      curve: 'straight'
+    },
+    title: {
+      text: 'Battery percentage',
+      align: 'center'
+    },
+    grid: {
+      row: {
+        colors: ['#012BFE', 'transparent'], 
+        opacity: 0.6
+      },
+    },
+    xaxis: {
+      categories: this.state.mapDat,
+    }
+  }
+
+    
   return (
     <div className={this.classes.root}>
     
     {/* Section recherche  */}
     <Container style={{minWidth:'1200px', maxWidth:'1200px', backgroundColor: '#FAFBFC',height: '100%' }}>
     
-    
+
     <Card>
-    <CardContent>
+    <CardContent>   
     <div>
                <img style={{float:"left", marginTop:"0.5em"}} width="40" src="./images/carrier.svg"/>
                <img style={{float:"right", marginTop:"0.5em"}} width="50" src="./images/back.png" onClick={() => this.props.callBackRetourTableauDeBordAide() }/>
@@ -75,7 +117,8 @@ class PageAide extends React.Component {
                <Typography style={{fontSize:"14px"}} color="textSecondary">
                  description
                </Typography>
-               </div>
+             
+   </div>
     
     <Grid container spacing={2}>
       <Grid item xs={12} >
@@ -105,9 +148,7 @@ class PageAide extends React.Component {
             subheader="Évolution du taux d'occupation du CPU"
           />
               <CardContent>
-              { (this.state.details != null ) && (
-               <TestLineChart data={this.state.details.cpu}/>
-              )}
+              
               </CardContent>
             </Card>
 
@@ -121,142 +162,7 @@ class PageAide extends React.Component {
             title="Mémoire"
             subheader="Évolution du taux d'occupation de la mémoire"
           />
-               { (this.state.details != null ) && (
-               <TestLineChart data={this.state.details.mem}/>
-              )}
-              </CardContent>
-            </Card>
-
-            <Card style={{marginTop:"1em"}}>
-              <CardContent>
-              <CardHeader
-            avatar={
-              <img width="24" src="./images/heat.svg"/>
-            }
-            
-            title="Température"
-            subheader="Évolution de la température"
-          />
-               <TestLineChart data={[{date:"01/01/2020", valeur:32}, {date:"02/01/2020", valeur:33}, {date:"03/01/2020", valeur:34},{date:"04/01/2020", valeur:30},{date:"05/01/2020", valeur:29},{date:"06/01/2020", valeur:34},{date:"07/01/2020", valeur:32},{date:"08/01/2020", valeur:31}]}/>
-              </CardContent>
-            </Card>
-       </Grid>
-
-          <Grid item xs={4} >
-              <Table>
-              <TableRow>
-                    <TableCell><img  width="30" src="./images/ip-address.svg"/></TableCell>
-                    <TableCell align="right">
-                    <Typography style={{fontSize:"18px"}} >
-                        {this.state.robot}
-                    </Typography>
-                    </TableCell>
-                </TableRow>
-                <TableRow>
-                    <TableCell><img  width="30" src="./images/usb.svg"/></TableCell>
-                    <TableCell align="right">
-                    <Typography style={{fontSize:"18px"}} >
-                        N/C
-                    </Typography>
-                    </TableCell>
-                </TableRow>
-                <TableRow>
-                    <TableCell><img  width="30" src="./images/cpu.svg"/></TableCell>
-                    <TableCell align="right">
-                    <Typography style={{fontSize:"18px"}} >
-                    {this.state.robot} %
-                    </Typography>
-                    </TableCell>
-                </TableRow>
-                <TableRow>
-                    <TableCell><img  width="30" src="./images/memory.svg"/></TableCell>
-                    <TableCell align="right">
-                    <Typography style={{fontSize:"18px"}} >
-                    {this.state.robot} %
-                    </Typography>
-                    </TableCell>
-                </TableRow>
-                <TableRow>
-                    <TableCell><img  width="30" src="./images/heat.svg"/></TableCell>
-                    <TableCell align="right">
-                    <Typography style={{fontSize:"18px"}} >
-                        N/C
-                    </Typography>
-                    </TableCell>
-                </TableRow>
                
-              </Table>
-          </Grid>
-
-        </Grid> 
-
-        
-
-        </CardContent>
-    
-     </Grid>
-
-     <Grid item xs={12} >
-        <Card style={{marginTop:"2em"}}>
-
-        <CardHeader
-            avatar={
-              <img width="38" src="./images/settings.svg"/>
-            }
-            
-            title="Mécanique cobot"
-            subheader="informations sur la partie mécanique"
-          />
-        </Card>
-        <CardContent>
-        <Grid container spacing={2}>
-
-        <Grid item xs={8} >
-            
-            <Card>
-           <CardHeader
-            avatar={
-              <img width="24" src="./images/heat.svg"/>
-            }
-            
-            title="Température"
-            subheader="Évolution de la température moteur"
-          />
-              <CardContent>
-               
-               <TestLineChart data={[{date:"01/01/2020", valeur:10}, {date:"02/01/2020", valeur:8}, {date:"03/01/2020", valeur:9},{date:"04/01/2020", valeur:6},{date:"05/01/2020", valeur:2},{date:"06/01/2020", valeur:9},{date:"07/01/2020", valeur:12},{date:"08/01/2020", valeur:4}]}/>
-              </CardContent>
-            </Card>
-
-            <Card style={{marginTop:"1em"}}>
-              <CardContent>
-              <CardHeader
-            avatar={
-              <img width="24" src="./images/wheel.svg"/>
-            }
-            
-            title="Roue droite"
-            subheader="Évolution de la vitesse de la roue droite"
-          />
-               { (this.state.details != null ) && (
-               <TestLineChart data={this.state.details.wheelR}/>
-              )}
-              </CardContent>
-            </Card>
-
-            <Card style={{marginTop:"1em"}}>
-              <CardContent>
-              <CardHeader
-            avatar={
-              <img width="24" src="./images/wheel.svg"/>
-            }
-            
-            title="Roue gauche"
-            subheader="Évolution de la vitesse de la roue gauche"
-          />
-                { (this.state.details != null ) && (
-               <TestLineChart data={this.state.details.wheelL}/>
-              )}
               </CardContent>
             </Card>
 
@@ -270,76 +176,20 @@ class PageAide extends React.Component {
             title="Batterie"
             subheader="Évolution de la batterie"
           />
-                { (this.state.details != null ) && (
-               <TestLineChart data={this.state.details.bat}/>
-              )}
+                
+               <ReactApexChart options={options} series={series} type="line" height={350} />
+          
               </CardContent>
             </Card>
        </Grid>
 
-          <Grid item xs={4} >
-              <Table>
-               
-                <TableRow>
-                    <TableCell><img  width="30" src="./images/heat.svg"/></TableCell>
-                    <TableCell align="right">
-                    <Typography style={{fontSize:"18px"}} >
-                        N/C
-                    </Typography>
-                    </TableCell>
-                </TableRow>
-                <TableRow>
-                    <TableCell><img  width="30" src="./images/wheel.svg"/></TableCell>
-                    <TableCell align="right">
-                    <Typography style={{fontSize:"18px"}} >
-                    {this.state.robot} tr/min
-                    </Typography>
-                    </TableCell>
-                </TableRow>
-                <TableRow>
-                    <TableCell><img  width="30" src="./images/wheel.svg"/></TableCell>
-                    <TableCell align="right">
-                    <Typography style={{fontSize:"18px"}} >
-                    {this.state.robot} tr/min
-                    </Typography>
-                    </TableCell>
-                </TableRow>
-                <TableRow>
-                    <TableCell><img  width="30" src="./images/car-battery.svg"/></TableCell>
-                    <TableCell align="right">  <Typography style={{fontSize:"18px"}} >
-                    {this.state.robot} %
-                    </Typography></TableCell>
-                </TableRow>
-                
-               
-              </Table>
-          </Grid>
         </Grid> 
-        </CardContent>
-     </Grid>
-    
-     <Grid item xs={12} >
-        <Card style={{marginTop:"2em"}}>
 
-        <CardHeader
-            avatar={
-              <img width="38" src="./images/log-format.svg"/>
-            }
-            
-            title="Journal des évènements"
-            subheader="Logs techniques"
-          />
-        </Card>
-        <CardContent>
-            <Table>
-            <TableRow>
-                <TableCell></TableCell>
-                <TableCell></TableCell>
-                <TableCell></TableCell>
-              </TableRow>
-            </Table>
+        
+
         </CardContent>
-      </Grid>
+    
+     </Grid>
 
     </Grid>
     
